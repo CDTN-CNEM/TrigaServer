13b80fc383c03b2fccad3268ffa13807722c6f0d
Álan Crístoffer <acristoffers@startmail.com>
2024-03-25 22:16:33 -0300

    fix: make nix build work correctly

--- flake.nix
+++ flake.nix
@@ -5,30 +5,45 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
-    spu.url = github:CDTNuclear/libModbusSystematomSPU;
+    spu.url = "github:SEURT-CDTN/libModbusSystematomSPU";
     spu.flake = false;
 
-    plc.url = github:CDTNuclear/libModbusMaestecPLC;
+    plc.url = "github:SEURT-CDTN/libOpcTrigaPLC";
     plc.flake = false;
+
+    open26541pp.url = "https://github.com/open62541pp/open62541pp?submodules=true";
+    open26541pp.flake = false;
+    open26541pp.type = "git";
+    open26541pp.submodules = true;
   };
 
-  outputs = { self, nixpkgs, flake-utils, spu, plc, ... }:
+  outputs = { nixpkgs, flake-utils, spu, plc, open26541pp, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        buildInputs = with pkgs; [
+          boost
+          cxxopts
+          jsoncpp
+          libmodbus
+          libserialport
+        ];
+        nativeBuildInputs = with pkgs; [
+          cmake
+          pkg-config
+          python3
+          stdenv
+        ];
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            boost
-            cmake
-            cxxopts
-            jsoncpp
-            libmodbus
-            libserialport
-            pkg-config
-            stdenv
-          ];
+          buildInputs = with pkgs;
+            [
+              busybox
+              git
+            ]
+            ++ buildInputs
+            ++ nativeBuildInputs;
         };
 
         packages.default = pkgs.stdenv.mkDerivation {
@@ -37,19 +52,11 @@
           cmakeFlags = [
             "-DDONT_INSTALL_CONF=1"
             "-DFETCHCONTENT_SOURCE_DIR_LIBMODBUSSYSTEMATOMSPU=${spu}"
-            "-DFETCHCONTENT_SOURCE_DIR_LIBMODBUSMAESTECPLC=${plc}"
+            "-DFETCHCONTENT_SOURCE_DIR_LIBOPCTRIGAPLC=${plc}"
+            "-DFETCHCONTENT_SOURCE_DIR_OPEN62541PP=${open26541pp}"
           ];
-          nativeBuildInputs = with pkgs; [
-            cmake
-            pkg-config
-          ];
-          buildInputs = with pkgs;[
-            boost
-            cxxopts
-            jsoncpp
-            libmodbus
-            libserialport
-          ];
+          inherit nativeBuildInputs;
+          inherit buildInputs;
         };
       }
     );
